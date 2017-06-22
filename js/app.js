806f4d2bad4b118aef69e30a67f3c7b26e969f46
Robert <rhieger@nyc.rr.com>
2017-06-21 21:52:19 -0400

    App Now Displays Article Descriptions of Feed

--- js/app.js
+++ js/app.js
@@ -8,6 +8,35 @@
 
 $(document).ready(function()    {
 
-    console.log('Document Loaded!');
+    // console.log('Document Loaded!');
+
+    var diggURL = 'http://digg.com/api/news/popular.json';
+
+    var proxy = 'https://accesscontrolalloworiginall.herokuapp.com/';
+
+    var diggEndPoint = proxy + diggURL;
+
+    console.log(diggEndPoint);
+
+    // Make the request to Digg:
+
+    $.get(diggEndPoint, function(response)   {
+
+        // Log the response to the console.
+
+        console.log(response);
+
+        var feed = response.data.feed;
+
+        for (var i = 0; i < response.data.feed.length; i ++)  {
+
+            $('main').append('<article><p>' + feed[i].content.description + '</p></article>');
+
+        }
+
+        console.log(feed);
+
+    });
+
 
 }); // end $(document).ready()
