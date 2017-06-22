1ebd6c6e41a47f7c5b5830ca4b1fce299152b717
Robert <rhieger@nyc.rr.com>
2017-06-21 20:06:17 -0400

    Added App Skeleton

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// app.js
+//
+// Robert Hieger
+//
+// In-Class Lab
+//
+// June 21, 2017
+
+$(document).ready(function()    {
+
+    console.log('Document Loaded!');
+
+}); // end $(document).ready()
